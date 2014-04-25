114aac8430ca19125040e7b72049b0e2aae285f4
Jiloko <gwenael.larmet@gmail.com>
2014-04-25 17:23:55 +0200

    recherche de lettre dans un graphe

--- Maillon.rb
+++ Maillon.rb
@@ -29,10 +29,10 @@ class Maillon
 			lettre
 		else
 			point = trace.shift
+			maillon = Maillon.new(point)
 			min = 500 #distance max (entre 0-0 et 100-100) est de 141,...
-			for arcs.each do |a|
-				tmp = point.distPoints(p)
-				puts tmp
+			arcs.each do |a|
+				tmp = a.m_arr.p.distPoints(point)
 				if tmp < min
 					min = tmp
 					maillon = a.m_arr
@@ -45,51 +45,39 @@ class Maillon
 
 	if __FILE__ == $0
 		#Graphe temporaire
-		p0 = Point.new(0,100)
-		p1 = Point.new(50,50)
-		p2 = Point.new(100,100)
-		p3 = Point.new(100,0)
-		p4 = Point.new(0,0)
-		p5 = Point.new(50,50)
-		p6 = Point.new(-1,-1)	
+
+		g = Maillon.new(Point.new(-1,-1))
+		m0 = Maillon.new(Point.new(0,100))
+		m1 = Maillon.new(Point.new(100,0))
+		m2 = Maillon.new(Point.new(50,50))
+		m3 = Maillon.new(Point.new(100,100))
+		m4 = Maillon.new(Point.new(0,0))
+		m5 = Maillon.new(Point.new(50,50))
 
 
-		g = Maillon.new(p0)
-		m1 = Maillon.new(p1)
-		m2 = Maillon.new(p2)
-		m3 = Maillon.new(p3)
-		m4 = Maillon.new(p4)
-		m5 = Maillon.new(p5)
-		m6 = Maillon.new(p6)
+		m2.lettre = 'q'
+		m3.lettre = 'u'
+		m4.lettre = 'a'
+		m5.lettre = 'c'
 
-		m3.lettre = 'q'
-		m4.lettre = 'u'
-		m5.lettre = 'a'
-		m6.lettre = 'c'
-
-		a1 = Arc.new(g,m1,0)
-		a2 = Arc.new(g,m2,0)
-		a3 = Arc.new(m1,m3,0)
-		a4 = Arc.new(m1,m4,0)
-		a5 = Arc.new(m2,m5,0)
-		a6 = Arc.new(m2,m6,0)
-
-		g.addArc(a1)
-		g.addArc(a2)
-		m1.addArc(a3)
-		m1.addArc(a4)
-		m2.addArc(a5)
-		m2.addArc(a6)
+		g.addArc(Arc.new(g,m0,0))
+		g.addArc(Arc.new(g,m1,0))
+		m0.addArc(Arc.new(m0,m2,0))
+		m0.addArc(Arc.new(m0,m3,0))
+		m1.addArc(Arc.new(m1,m4,0))
+		m1.addArc(Arc.new(m1,m5,0))
 
 		#Fin définition graphe temporaire
 
 
-		trace = [Point.new(10,5),Point.new(24,57)]
-		#puts g.solve(trace)
-		trace = [Point.new(50,50),Point.new(75,75)]
-		#puts g.solve(trace)
+		trace = [Point.new(0,100),Point.new(50,50)]
+		puts g.solve(trace) #==> Q
+		trace = [Point.new(0,100),Point.new(100,100)]
+		puts g.solve(trace) #==> U
 		trace = [Point.new(100,0),Point.new(0,0)]
-		#puts g.solve(trace)
-		#puts p0.distPoints(p2)
+		puts g.solve(trace) #==> Q
+		trace = [Point.new(100,0),Point.new(50,50)]
+		puts g.solve(trace) #==> Q
+		
 	end
 end
