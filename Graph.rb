7a0b47b6d72ff37d73ca50014f4481a50d8760c3
Jiloko <gwenael.larmet@gmail.com>
2014-04-27 14:35:57 +0200

    Modification de Graph.rb

--- Graph.rb
+++ Graph.rb
@@ -1,5 +1,11 @@
 #!/usr/bin/env ruby
 
 class Graph
-  
+	def buildGraph
+		#choper les lettres
+
+		#Construire le graphe depuis les lettres
+
+		#renvoi le maillon de tete du graphe construit
+	end
 end
