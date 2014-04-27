222ca52140ea1a01426424993332b489023f7fca
Ben <benjamin@herb.fr>
2014-04-27 14:47:33 +0200

    Ajout lineEdit

--- Main.rb
+++ Main.rb
@@ -4,6 +4,9 @@ require 'thread'
 require 'Qt'
 require './DrawBox'
 
+WIDTH = 550
+HEIGHT = 350
+
 class QtApp < Qt::Widget
 	def initialize
 		super
@@ -33,20 +36,21 @@ class QtApp < Qt::Widget
 		grid = Qt::GridLayout.new self
 
 		windLabel = Qt::Label.new "Draw here :", self
-		@label = Qt::Label.new "Pos : ", self
+		@result = Qt::LineEdit.new self
 		gv = DrawBox.new self
 		quit = Qt::PushButton.new 'Quit', self
 		
-		grid.addWidget windLabel, 0, 0
-		grid.addWidget gv, 1, 0, 2, 4
-		grid.addWidget @label, 4, 0
-		grid.addWidget quit, 4, 3
+        grid.addWidget windLabel, 0, 0
+        grid.addWidget gv, 1, 0, 2, 4
+		grid.setColumnStretch 1, 1
+		grid.addWidget @result, 4, 0, 1, 2
+		grid.addWidget quit, 4, 2
 
 		connect quit, SIGNAL('clicked()'), $qApp, SLOT('quit()')
 	end
 
-	def getLabel
-		return @label
+	def getResult
+		return @result
 	end
 end
 
