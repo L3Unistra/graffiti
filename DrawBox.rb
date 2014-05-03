7ae14cea1035eba8c61294f3b04ce4d181286822
Ben <benjamin@herb.fr>
2014-05-03 15:31:20 +0200

    ajout shift, space, backspace

--- DrawBox.rb
+++ DrawBox.rb
@@ -4,21 +4,22 @@ require 'thread'
 require 'Qt'
 require './Graph'
 require './Point'
-#require './TraceConverter'
 
 class DrawBox < Qt::Widget
 	def initialize(parent, mode)
 		super parent
 
-		
-		@g = mode == 1 ? Graph.new("alphabet.json") : Graph.new("numbers.json")
+		@graph = mode == 1 ? Graph.new("alphabet.json") : Graph.new("numbers.json")
 
 		@result
+
+		@shift = false
 
 		@pos1
 		@pos2
 
 		@parent = parent
+		@r = @parent.getResult
 
 		@image = Qt::Image.new 1000, 1000, 7
 		@image.fill Qt::Color.new "#ffffff"
@@ -38,7 +39,6 @@ class DrawBox < Qt::Widget
 		if (e.pos.x > 0 and e.pos.x < @image.width) and (e.pos.y > 0 and e.pos.y < @image.height)
 			@pos2 = e.pos
 			@result << Point.new(@pos2.x, @pos2.y)
-			#puts "Pos : x : #{@pos1.x}, y : #{@pos1.y}"
 			drawLineTo @pos1, @pos2
 			#drawPoint @pos1
 			@pos1 = @pos2
@@ -53,16 +53,28 @@ class DrawBox < Qt::Widget
 
 	def mouseReleaseEvent(e)
 		@image.fill Qt::Color.new "#ffffff"
-		# @result.each{|p| puts p}
 
 		if @result.length > 19
-			r = @parent.getResult
-			r.insert(@g.solve(@result))
+			char = @graph.solve(@result)
+			if char == "shift"
+				@shift = true
+			elsif char == "back"
+				@r.backspace
+			else
+				if @shift
+					@r.insert(char.upcase)
+					@shift = false
+				else
+					@r.insert(char)
+				end
+			end
 		end
+
+		# lignes pour la création du json
 
 		# puts @result.length
 		# tc = TraceConverter.new(@result)
-  		#tab = tc.resize
+		# tab = tc.resize
 
 		# file = File.open('new.json', 'a')
 		# str = '{"letter":"a", "points":['
@@ -72,6 +84,8 @@ class DrawBox < Qt::Widget
 
 		# file.puts str 
 		# file.close
+
+		
 		update 
 	end
 
@@ -80,36 +94,36 @@ class DrawBox < Qt::Widget
 		p.begin @image
 		p.setRenderHint Qt::Painter::Antialiasing
 
-        color = Qt::Color.new
-        color.setNamedColor "#333333"
+		color = Qt::Color.new
+		color.setNamedColor "#333333"
 
-        pen = Qt::Pen.new color
-        pen.setWidth 3
-        p.setPen pen
+		pen = Qt::Pen.new color
+		pen.setWidth 3
+		p.setPen pen
 
-        p.drawLine Qt::Line.new(pos1, pos2)
-        rad = (3/2)+2;
-        update(Qt::Rect.new(pos1, pos2).normalized().adjusted(-rad, -rad, +rad, +rad))
-        p.end
+		p.drawLine Qt::Line.new(pos1, pos2)
+		rad = (3/2)+2;
+		update(Qt::Rect.new(pos1, pos2).normalized().adjusted(-rad, -rad, +rad, +rad))
+		p.end
 	end
 
-	def drawPoint pos1
-		p = Qt::Painter.new
-		p.begin @image
-		p.setRenderHint Qt::Painter::Antialiasing
+	# def drawPoint pos1
+	# 	p = Qt::Painter.new
+	# 	p.begin @image
+	# 	p.setRenderHint Qt::Painter::Antialiasing
 
-        color = Qt::Color.new
-        color.setNamedColor "#333333"
+	# 	color = Qt::Color.new
+	# 	color.setNamedColor "#333333"
 
-        pen = Qt::Pen.new color
-        pen.setWidth 3
-        p.setPen pen
+	# 	pen = Qt::Pen.new color
+	# 	pen.setWidth 3
+	# 	p.setPen pen
 
-        p.drawPoint pos1.x, pos1.y
-        rad = (3/2)+2;
-        update
-        p.end
-	end
+	# 	p.drawPoint pos1.x, pos1.y
+	# 	rad = (3/2)+2;
+	# 	update
+	# 	p.end
+	# end
 
 	def resize
 		@image = Qt::Image.new @parent.width/2, @parent.width/2, 7
